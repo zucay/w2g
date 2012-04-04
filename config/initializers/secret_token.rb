822cbd9cd94e86cd4cfbc162a62114a6be6b6c57
yukihico <yukihico@gmail.com>
2012-04-04 15:10:59 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+W2g::Application.config.secret_token = '7664d5aa6e551c0ae9de06865b6d90a0d74ca46a44b422e95a68b4f6e8107b3bea08c9cc42721b9c5f63e584104191dad8f8ae9b90a601cec87b10eb3d73ee35'
