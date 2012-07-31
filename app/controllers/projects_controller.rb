9e541865e3d95cd1e0aa516e02b60d987fd9738a
yukihico <yukihico@gmail.com>
2012-07-31 16:40:58 +0900

    change routes

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -3,4 +3,7 @@ class ProjectsController < InheritedResources::Base
   def select_layout
     'twitter'
   end
+  def index
+    redirect_to project_url(Project.order('created_at').last)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -84,7 +84,8 @@ W2g::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   #root :to => 'pointing#index'
-  root :to => 'admin/dashboard#index'
+  #root :to => 'admin/dashboard#index'
+  root :to => 'projects#index'
 
   # See how all your routes lay out with "rake routes"
 
